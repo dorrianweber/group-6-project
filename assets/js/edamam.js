2714eb3a23c59cd54fedf84d4242194250ab4c44
Dorrian Weber <dorrianweber@gmail.com>
2021-02-04 00:14:15 -0800

    Recipe table is now cleared before adding content; added alt text for thumbnail images in recipe table; app ID for Edamam API call is now stored in a variable; added an input field for number of search results which user must fill out before searching for recipes

--- assets/js/edamam.js
+++ assets/js/edamam.js
@@ -1,33 +1,39 @@
 // JavaScript file for Edamam API
 var endpoint = "https://api.edamam.com/search";
 var apiKey = "dce91a1b6d878956479351767c573add";
+var appID = "36d96c48";
 
-numberResults = 0;
 calMin = 0;
 calMax = 4000;
 
+
 // URL to work w/
-// + "&from=0&to=" + numberResults + "&calories=" + calMin + "-" + calMax + "&health=alcohol-free"
+// + "&from=0&to=" + "&calories=" + calMin + "-" + calMax + "&health=alcohol-free"
 
 function apiCall(){
-    // Sets ingredient keyword to whatever is in the search bar
+    // Sets ingredient keyword
     var ingKeyword = $("#searchBar").val();
 
+    // Sets max number of results returned
+    var numberResults = $("#numberResults").val();
+
     // Fetch call for Edamam API
-    fetch(endpoint + "?q=" + ingKeyword + "&app_id=36d96c48&app_key=" + apiKey + "&from=0&to=15")
+    fetch(endpoint + "?q=" + ingKeyword + "&app_id=" + appID + "&app_key=" + apiKey + "&from=0&to=" + numberResults)
 
     .then(function (response) {
         return response.json();
     })
 
     .then(function (data){
+        // Logs result of API call in console
         console.log(data);
 
+        // Populates table with recipes & thumbnail images
         for (var i = 0; i < data.hits.length; i++){
             var tableRow = $("<tr>");
             var tableData = $("<td>");
             var link = $("<a>").attr("href", data.hits[i].recipe.url).text(data.hits[i].recipe.label);
-            var thumbnail = $("<img>").attr("src", data.hits[i].recipe.image).addClass("thumbnail");
+            var thumbnail = $("<img>").attr("src", data.hits[i].recipe.image).attr("alt", "Photo of " + data.hits[i].recipe.label).addClass("thumbnail");
 
             tableData.append(thumbnail);
             tableData.append(link);
@@ -38,5 +44,15 @@ function apiCall(){
 
 // When the "find recipes" button is clicked...
 $("#recipes-button").click(function(){
+    // Clears out recipe table
+    $("#recipes-table").empty();
+
+    // Calls Edamam API
     apiCall();    
 });
+
+// Since last commit:
+//      -Clear recipe table
+//      -Thumbnail alt text
+//      -Put app ID into a var
+//      -Number results input
