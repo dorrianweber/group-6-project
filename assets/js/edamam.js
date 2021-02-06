8367a805c51a197a2e0bf0a5a9bfc5846f2d6f59
Dorrian Weber <dorrianweber@gmail.com>
2021-02-06 10:37:44 -0800

    Fixed scoping issues with ingKeyword variable by breaking up the building of the API call URL into multiple areas

--- assets/js/edamam.js
+++ assets/js/edamam.js
@@ -3,6 +3,15 @@ var endpoint = "https://api.edamam.com/search";
 var apiKey = "dce91a1b6d878956479351767c573add";
 var appID = "36d96c48";
 
+// Starting point of API call URL
+var apiURL = endpoint;
+
+// Parameter section for API URL
+var apiParameters = "?q=";
+
+// Finishes API URL with app ID & key
+var apiEnd = "&app_id=" + appID + "&app_key=" + apiKey;
+
 // Stays yes even after you unlick and search again, always puts &glutenfree in API call
 // $("#glutenFree").val("no");
 
@@ -18,18 +27,8 @@ var appID = "36d96c48";
 
 // console.log($("#glutenFree").val());
 
-// URL to work w/
-// + "&from=0&to=" + "&calories=" + calMin + "-" + calMax + "&health=alcohol-free"
-
+// Function for calling API
 function apiCall(){
-    // Sets ingredient keyword
-    var ingKeyword = $("#searchBar").val();
-
-    // Sets max number of results returned
-    var numberResults = $("#numberResults").val();
-
-    // Building API call URL
-    var apiURL = endpoint + "?q=" + ingKeyword + "&app_id=" + appID + "&app_key=" + apiKey;
     
     // console.log($("#glutenFree").val());
     
@@ -38,40 +37,33 @@ function apiCall(){
     //     apiURL = apiURL + "&glutenfree";
     // };
 
-    // Conditional for number of results
-    if (numberResults !== "") {
+    // Updates API URL to include all necessary parts
+    apiURL = apiURL + apiParameters + apiEnd;
 
-        // Fetch call for Edamam API
-        fetch(apiURL + "&from=0&to=" + numberResults)
+    // Fetch call for Edamam API
+    fetch(apiURL)
 
-        .then(function (response) {
-            return response.json();
-        })
+    .then(function (response) {
+        return response.json();
+    })
 
-        .then(function (data){
-            // Logs result of API call in console
-            console.log(data);
+    .then(function (data){
+        // Logs result of API call in console
+        console.log(data);
 
-            // Populates table with recipes & thumbnail images
-            for (var i = 0; i < data.hits.length; i++){
-                var tableRow = $("<tr>");
-                var tableData = $("<td>");
-                var link = $("<a>").attr("href", data.hits[i].recipe.url).text(data.hits[i].recipe.label);
-                var thumbnail = $("<img>").attr("src", data.hits[i].recipe.image).attr("alt", "Photo of " + data.hits[i].recipe.label).addClass("thumbnail");
+        // Populates table with recipes & thumbnail images
+        for (var i = 0; i < data.hits.length; i++){
+            var tableRow = $("<tr>");
+            var tableData = $("<td>");
+            var link = $("<a>").attr("href", data.hits[i].recipe.url).text(data.hits[i].recipe.label);
+            var thumbnail = $("<img>").attr("src", data.hits[i].recipe.image).attr("alt", "Photo of " + data.hits[i].recipe.label).addClass("thumbnail");
 
-                tableData.append(thumbnail);
-                tableData.append(link);
-                tableRow.append(tableData);
-                $("#recipes-table").append(tableRow);
-            };
-        });
-
-    }
-
-    else {
-        var errorMessage = $("<h4>").attr("id", "errorMessage").text("Please choose how many results you would like");
-        $("#userInputs").append(errorMessage);
-    };
+            tableData.append(thumbnail);
+            tableData.append(link);
+            tableRow.append(tableData);
+            $("#recipes-table").append(tableRow);
+        };
+    });
 
 };
 
@@ -79,7 +71,30 @@ function apiCall(){
 $("#recipes-button").click(function(){
     // Clears out recipe table
     $("#recipes-table").empty();
+    
+    // Sets max number of results returned
+    var numberResults = $("#numberResults").val();
 
-    // Calls Edamam API
-    apiCall();    
+    // As long as number of results input is not blank...
+    if (numberResults !== "") {
+
+        // Sets ingredient keyword
+        var ingKeyword = $("#searchBar").val();
+
+        // Updates API parameters section to include ingredient keyword
+        apiParameters = apiParameters + ingKeyword;
+
+        // Updates API parameters section to include number of results
+        apiParameters = apiParameters + "&from=0&to=" + numberResults;
+        
+        // Calls Edamam API
+        apiCall();    
+    }
+    
+    // Otherwise, displays an error message
+    else {
+        var errorMessage = $("<h4>").attr("id", "errorMessage").text("Please choose how many results you would like");
+        $("#userInputs").append(errorMessage);
+    };
+
 });
