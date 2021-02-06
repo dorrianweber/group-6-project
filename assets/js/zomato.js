4003de6b8029053dc45abc6522e473342e13d86e
Reynaldo Bracamonte <reybrac@yahoo.com>
2021-02-06 12:10:07 -0800

    Added city search section and second api to look up the city ID. Linked the search food section, so that you can now update the type of restaurants. Added alerts if the city is missing or the keyword is missing

--- assets/js/zomato.js
+++ assets/js/zomato.js
@@ -3,16 +3,40 @@
 //curl -X GET --header "Accept: application/json" --header "user-key: 81567c6d0a81c709e1edf53310578e0c" "https://developers.zomato.com/api/v2.1/restaurant?res_id=16774318"
 
 $(document).ready(function() {
-    var entityID = "499";
-    var entityType = "city";
-    var typeFood = "mexican";
-    // var numberResults = "10";
-    var tableBody = document.getElementById('restaurant-table');
         
-    // AJAX call for Zomato API
-    function ajaxCall(){
+    var tableBody = document.getElementById('restaurant-table');
+    
+    function ajaxCall1(){
+        var cityName = $("#searchCity").val();
+        if(cityName===""){
+            alert("Please enter the name of a city");
+            return;
+        }
+        console.log("cityName", cityName);
         $.ajax({
-            url: "https://developers.zomato.com/api/v2.1/search?entity_id=" + entityID + "&entity_type=" + entityType + "&q=" + typeFood + "%20food",
+            url: "https://developers.zomato.com/api/v2.1/locations?query=" + cityName,
+            method: "GET",
+            headers: {
+                "user-key": "81567c6d0a81c709e1edf53310578e0c",
+                "Content-type": "application/json"
+            },
+            
+    //     // When call is finished...
+        }).done(function (res1){
+            
+            var x = res1.location_suggestions[0].entity_id;
+            console.log("res1: ", res1);
+            console.log("entity ID: ", res1.location_suggestions);
+            console.log("x: ", x);
+            ajaxCall2(x);
+        }); 
+    };
+    
+    // AJAX call for Zomato API restaurants
+    function ajaxCall2(x){
+        var typeFood = $("#searchBar").val();
+        $.ajax({
+            url: "https://developers.zomato.com/api/v2.1/search?entity_id=" + x + "&entity_type=city&q=" + typeFood + "%20food",
             method: "GET",
             headers: {
                 "user-key": "81567c6d0a81c709e1edf53310578e0c",
@@ -23,27 +47,29 @@ $(document).ready(function() {
         }).done(function (res){
 
             // Log the information in the console
-            console.log(res);
-            
+            console.log("Food response: ", res);
+            // if(typeFood===""){
+            //     alert("Please enter a type of food");
+            //     return;
+            //     }
             // For loop to populate the table with restaurant information
             for (var i = 0; i < res.restaurants.length; i++){
                 var createTableRow = document.createElement('tr');
                 var tableData = document.createElement('td');
                 var link = document.createElement('a');
-                var pic = document.createElement('img');
+                //var rating = $('a').text(res.restaurants[i].restaurant.user_rating.aggregate_rating);
+               
 
                 link.textContent = res.restaurants[i].restaurant.name;
-                link.href = res.restaurants[i].restaurant.events_url;
-                pic.textContent = res.restaurants[i].restaurant.photos_url;
-                pic.href = res.restaurants[i].restaurant.photos_url;
+                link.href = res.restaurants[i].restaurant.events_url;            
                 
-    
+
                 tableData.appendChild(link);
-                tableData.appendChild(pic);
+                //tableData.appendChild(rating);
 
                 createTableRow.appendChild(tableData);
                 tableBody.appendChild(createTableRow);
-                $("#restaurant-table").append(res[i])
+                $("#restaurant-table").append(res[i]);
             };
         });  
     };
@@ -51,8 +77,12 @@ $(document).ready(function() {
     // When the "restaurants button" is clicked...
     $("#restaurants-button").click(function(){
         // Run "ajaxCall" function
-        ajaxCall(); 
+        //add if statement, check if blank, message enter city and type of food
+        
+        $("#restaurant-table").empty();
+        ajaxCall1();
+        // ajaxCall2(); 
+
     });
 });
 
-//$("#repo-table").append("hello");   
